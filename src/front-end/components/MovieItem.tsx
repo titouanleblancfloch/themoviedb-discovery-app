16605f2a0715fee674a8beac7ca008e487104403
titouanleblancfloch <titouan.leblanc.floch@etu.univ-poitiers.fr>
2026-09-21 16:25:15 +0000

    style: 🎨 refactor MovieItem component layout and enhance movie display

--- src/front-end/components/MovieItem.tsx
+++ src/front-end/components/MovieItem.tsx
@@ -5,12 +5,22 @@ type MovieItemProps = {
 };
 
 export default function MovieItem({ movie }: MovieItemProps) {
+  const releaseYear = movie.release_date.slice(0, 4);
+  const posterUrl = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w185${movie.poster_path}`
+    : null;
+  const rating = movie.vote_average.toFixed(1);
+
   return (
-    <li>
-      <h2>{movie.title}</h2>
-      <p>{movie.overview}</p>
-      <p>Release Date: {movie.release_date}</p>
-      <p>Rating: {movie.vote_average}</p>
-    </li>
+    <div>
+      {posterUrl ? <img src={posterUrl} alt={`Affiche de ${movie.title}`} /> : <div />}
+
+      <div>
+        <h2>{movie.title}</h2>
+        <p>
+          {releaseYear} · Note {rating}
+        </p>
+      </div>
+    </div>
   );
 }
