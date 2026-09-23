749415cba71861bdea9d3f08320cbf79f111dfcf
titouanleblancfloch <titouan.leblanc.floch@etu.univ-poitiers.fr>
2026-09-23 15:02:04 +0000

    feat: ✨ ajouter la mise en page et le style pour l'application, y compris le composant MovieItem

--- src/front-end/App.tsx
+++ src/front-end/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import type { Movie } from "../back-end/schemas/MoviesTypes";
 import MovieItem from "./components/MovieItem";
+import "./app.css";
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[] | null>(null);
@@ -12,18 +13,28 @@ export default function App() {
   }, []);
 
   return (
-    <div>
-      <h1>Popular Movies</h1>
-
-      {movies ? (
-        <ul>
-          {movies.map((movie) => (
-            <MovieItem key={movie.id} movie={movie} />
-          ))}
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+    <main className="app-shell">
+      <header className="app-header">
+        <h1>Films populaires</h1>
+        <h2>
+          Films tendances en France, d'après les données de <b>The Movie Database</b>
+        </h2>
+      </header>
+      <section>
+        {movies ? (
+          <ul className="movie-grid">
+            {movies.map((movie) => (
+              <li key={movie.id}>
+                <article>
+                  <MovieItem movie={movie} />
+                </article>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="status-message">Loading...</p>
+        )}
+      </section>
+    </main>
   );
 }
