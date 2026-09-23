e56b6e008af5f4885f79dc22929cc624d55667ae
titouanleblancfloch <titouan.leblanc.floch@etu.univ-poitiers.fr>
2026-09-23 15:23:24 +0000

    feat: ✨ add TMDB query parameter handling

---
+++ src/back-end/constants.ts
@@ -0,0 +1,8 @@
+// Langue par défaut pour les requêtes à l'API TMDB
+export const DEFAULT_LANGUAGE = "fr-FR";
+
+// Page par défaut pour les requêtes à l'API TMDB
+export const DEFAULT_PAGE = "1";
+
+// Région par défaut pour les requêtes à l'API TMDB
+export const DEFAULT_REGION = "FR";
